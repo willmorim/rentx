e493a57dba64f8e2400136f30f04aaa2b0916fae
William Amorim <pdbawilliam@gmail.com>
2022-02-07 17:35:45 -0300

    first commit

---
+++ src/components/BackButton/index.tsx
@@ -0,0 +1,21 @@
+import { Container } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import { BorderlessButtonProps } from "react-native-gesture-handler";
+
+interface Props extends BorderlessButtonProps {
+  color?: string;
+}
+
+export function BackButton({ color, ...rest }: Props) {
+  const theme = useTheme();
+  return (
+    <Container {...rest}>
+      <MaterialIcons
+        name="chevron-left"
+        size={24}
+        color={color ? color : theme.colors.text}
+      />
+    </Container>
+  );
+}
