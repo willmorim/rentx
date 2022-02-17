93cab9135c28353b7e309eed950354ba16fc03c9
William Amorim <pdbawilliam@gmail.com>
2022-02-16 23:20:15 -0300

    create some screens and components

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,31 @@
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
-import { Container, Header, CarImages } from "./styles";
+import { Accessory } from "../../components/Accessory";
+import { Button } from "../../components/Button";
+
+import speedSvg from "../../assets/speed.svg";
+import accelerationSvg from "../../assets/acceleration.svg";
+import forceSvg from "../../assets/force.svg";
+import gasolineSvg from "../../assets/gasoline.svg";
+import exchangeSvg from "../../assets/exchange.svg";
+import peopleSvg from "../../assets/people.svg";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from "./styles";
 
 export function CarDetails() {
   return (
@@ -16,6 +41,49 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory icon={speedSvg} name="380Km/h" />
+          <Accessory icon={accelerationSvg} name="3.2s" />
+          <Accessory icon={forceSvg} name="800 HP" />
+          <Accessory icon={gasolineSvg} name="Gasolina" />
+          <Accessory icon={exchangeSvg} name="Auto" />
+          <Accessory icon={peopleSvg} name="2 pessoas" />
+        </Accessories>
+
+        <About>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer
+        </About>
+        <About>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer
+        </About>
+        <About>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer
+        </About>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,66 @@
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+
+import ArrowSvg from "../../assets/arrow.svg";
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  DateValueContainer,
+  Content,
+  Footer,
+} from "./styles";
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma{"\n"}data de inicio e{"\n"} fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue>18/07/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValueContainer selected={false}>
+              <DateValue>18/07/2022</DateValue>
+            </DateValueContainer>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar></Calendar>
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
