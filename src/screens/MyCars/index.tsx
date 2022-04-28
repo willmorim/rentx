126f923b691f71be8c6f126abf4d5529c5ac2184
William Amorim <pdbawilliam@gmail.com>
2022-04-28 12:00:06 -0300

    feat: reusing car component

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from "react";
 import { StatusBar, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { useTheme } from "styled-components";
+import { AntDesign } from '@expo/vector-icons';
 
 import { BackButton } from "../../components/BackButton";
+import { Load } from "../../components/Load";
 import { CarDTO } from "../../dtos/CarDTO";
 import { Car } from "../../components/Car";
 
@@ -18,12 +20,19 @@ import {
   Appointments,
   AppointmentsTitle,
   AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
 } from "./styles";
 
 interface CarProps {
   id: string;
   user_id: string;
   car: CarDTO;
+  startDate: string;
+  endDate: string;
 }
 
 export function MyCars() {
@@ -66,19 +75,38 @@ export function MyCars() {
 
         <SubTitle>Conforto, segurança e praticidade.</SubTitle>
       </Header>
-      <Content>
-        <Appointments>
-          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
-          <AppointmentsQuantity>05</AppointmentsQuantity>
-        </Appointments>
+      {loading ? <Load/> :
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
 
-        <FlatList
-          data={cars}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => <Car data={item.car} />}
-        />
-      </Content>
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => 
+            <CarWrapper>
+              <Car data={item.car} />
+              <CarFooter>
+                <CarFooterTitle>Período</CarFooterTitle>
+                <CarFooterPeriod>
+                  <CarFooterDate>{item.startDate}</CarFooterDate>
+                  <AntDesign 
+                  name="arrowright"
+                  size={20}
+                  color={theme.colors.title}
+                  style={{marginHorizontal: 10}}
+                  />
+                  <CarFooterDate>{item.endDate}</CarFooterDate>
+                </CarFooterPeriod>
+              </CarFooter>
+            </CarWrapper>
+          }
+          />
+        </Content>
+       }
     </Container>
   );
 }
--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -76,7 +76,15 @@ export function SchedulingDetails() {
       ...dates,
     ];
 
-    // api.post("schedules_byuser");
+    await api.post("schedules_byuser", { 
+      user_id: 1,
+      car,
+      startDate: format(getPlatformDate(new Date(dates[0])), "dd/MM/yyyy"),
+      endDate: format(
+        getPlatformDate(new Date(dates[dates.length - 1])),
+        "dd/MM/yyyy"
+      )
+    });
 
     api
       .put(`/schedules_bycars/${car.id}`, {
